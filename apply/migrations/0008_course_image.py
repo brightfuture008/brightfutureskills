425dfca256a9c1586733c51f60c5ce675a6a2a7a
Your GitHub Username <brightfutureskills08@gmail.com>
2025-12-12 22:18:14 +0300

    layout modifications

--- apply/migrations/0008_course_image.py
+++ apply/migrations/0008_course_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('apply', '0007_remove_student_course_student_course'),
+        ('apply', '0007_auto_20251212_1845'),
     ]
 
     operations = [
