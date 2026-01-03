65d7bc28453ef8dbcb822d395377624d3b057552
Your GitHub Username <brightfutureskills08@gmail.com>
2026-01-03 17:50:26 +0300

    adding comments per course 1

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import CourseComment, ChatMessage
+from .models import ChatMessage, CourseComment
 
-admin.site.register(CourseComment)
-admin.site.register(ChatMessage)
+@admin.register(CourseComment)
+class CourseCommentAdmin(admin.ModelAdmin):
+    list_display = ('course', 'user', 'content', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at', 'course')
+    search_fields = ('content', 'user__username', 'course__title')
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'message', 'timestamp', 'is_read')
+    list_filter = ('is_read', 'timestamp')
+    search_fields = ('message', 'sender__username', 'receiver__username')
